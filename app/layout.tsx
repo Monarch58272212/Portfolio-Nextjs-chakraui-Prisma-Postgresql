60b728f6470ed9f1eb13afb7277bbcef80a3478e
Monarch <gusoaxcell@example.com>
2025-06-13 12:37:57 -0700

    hgfds

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import ChakraProviderWrapper from "./chakraProviders/providers";
 import Navigation from "./Navigation/Navigation";
 import { ColorModeScript } from "@chakra-ui/react";
 import theme from "./chakraProviders/theme";
-import { KindeProvider } from "@kinde-oss/kinde-auth-nextjs";
 
 export default function RootLayout({
   children,
@@ -17,12 +16,10 @@ export default function RootLayout({
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       </head>
       <body>
-        <KindeProvider>
-          <ChakraProviderWrapper>
-            <Navigation />
-            {children}
-          </ChakraProviderWrapper>
-        </KindeProvider>
+        <ChakraProviderWrapper>
+          <Navigation />
+          {children}
+        </ChakraProviderWrapper>
       </body>
     </html>
   );
